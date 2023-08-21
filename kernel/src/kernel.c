69e4fffcbf01465546e53fec6b10c4e91e803e11
Lautaro Robles <lrobles@frba.utn.edu.ar>
2023-08-21 23:07:56 +0000

    Como los logs ahora los vamos a guardar dentro de la carpeta logs dentro de cada modulo Hay que cambiar el .gitignore para no commitearlos

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,7 +5,7 @@ t_log *logger;
 
 int main(int argc, char *argv[])
 {
-    logger = log_create("./bin/kernel.log", "kernel", true, LOG_LEVEL_INFO);
+    logger = log_create("kernel/logs/kernel.log", "kernel", true, LOG_LEVEL_INFO);
 
     log_warning(logger, "Falta implementar el modulo kernel");
     log_trace(logger, "Se ejecuto el modulo kernel con %i parametros", argc - 1);
